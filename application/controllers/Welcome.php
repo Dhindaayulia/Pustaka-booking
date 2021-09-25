1728eb5741126f64cdfabb05e2011e2d867d0a87
Dhindaayulia <dhndaayuliaaa14@gmail.com>
2021-09-25 18:46:19 +0700

    pustaka-booking-3a

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function index()
+	{
+         echo"<h1>Perkenalkan</h1>";
+		 echo "Nama : Dhinda Ayulia Primadhoni <br>
+		 Tanggal Lahir : 28 Nov 2001<br>
+		 Tempat Tinggal : Bekasi<br>
+		 Motivasi Kuliah : Agar mempunyai wawasan yang luas dan 
+		 bisa mengangkat derajat orang tua di masa depan";
+
+
+		      
+		
+	}
+}
